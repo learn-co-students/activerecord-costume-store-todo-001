1bed0f6e96bd2fbba1b49fdec1fcecf255bb5739
Becckitt <hello@beccabarton.com>
2015-06-22 10:10:16 -0400

    Finished. Thumbs up for active record.

--- db/migrations/002_create_costume_stores.rb
+++ db/migrations/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration
+  def change
+    create_table :costume_stores do |s|
+      s.text :name
+      s.text :location
+      s.text :costume_inventory
+      s.integer :num_of_employees
+      s.boolean :still_in_business
+      s.datetime :opening_time
+      s.datetime :closing_time
+      
+    end
+  end
+end
