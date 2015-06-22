f61da3d77aeec8d72c9adcf6aecfd8a46b3c6c77
AnnaErshova <anna.ershova@gmail.com>
2015-06-22 09:37:19 -0400

    all done

--- db/migrations/001_create_costumes.rb
+++ db/migrations/001_create_costumes.rb
@@ -1,3 +1,15 @@
+class CreateCostumes < ActiveRecord::Migration
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.text :price
+      t.text :size
+      t.text :image_url
+      t.timestamps
+    end # end create_table
+  end # end change
+end # end class
+
 # Create a class and inherit from ActiveRecord::Migration
 
 # By convention, the class name should match the part of the
--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      t.text :name
+      t.string :location
+      t.text :theme
+      t.text :price
+      t.boolean :family_friendly
+      t.text :opening_date
+      t.text :closing_date
+      t.string :description
+    end # end create_table
+  end # end change
+end # end class
+
 # Create your haunted_houses migration here
