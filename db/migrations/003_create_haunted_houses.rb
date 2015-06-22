b98ee85d6dc4660f43f7db63ceb9fcbe1e47cb96
elainapolson <elainapolson@gmail.com>
2015-06-22 09:32:48 -0400

    done

--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |c|
+      c.string :name
+      c.string :location
+      c.string :theme
+      c.integer :price
+      c.boolean :family_friendly
+      c.date :opening_date
+      c.date :closing_date
+      c.text :description
+    end
+  end
+end
+
