93a2180de8b2528d1f953f65584db09ebeb768cc
Chris Aporta <christopher.aporta@gmail.com>
2015-12-06 21:27:56 -0500

    complete lab

--- db/migrations/001_create_costumes.rb
+++ db/migrations/001_create_costumes.rb
@@ -7,3 +7,15 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
+
+class CreateCostumes < ActiveRecord::Migration
+  def change
+    create_table :costumes do |t|
+      t.text        :name
+      t.decimal     :price
+      t.text        :image_url
+      t.text        :size
+      t.timestamps   null:true
+    end
+  end
+end
